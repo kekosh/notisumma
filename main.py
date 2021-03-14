ac362efcca35bd30b4b0731841b7d66b3ea365ea
kekosh <kenta0540@gmail.com>
2021-03-14 13:57:06 +0900

    エンコード不具合に対応、不具合内容取得処理追加

--- main.py
+++ main.py
@@ -13,23 +13,23 @@ def get_file_path_list(dir, *args):
     return _path_list
 
 # unzip compressed files
-def unzip(file_list, password, expansions_target):
+def unzip(file_list, password, extract_target):
     _password = bytes(password,encoding='utf-8')
 
     for file in file_list:
         with zipfile.ZipFile(file, 'r') as zip_file:
             for file in zip_file.infolist():
                 try:
-                    print(file.filename)
-                    print(file.orig_filename)
-
-                    file.filename = file.filename.encode('cp437').decode('cp932')
-                    print(file.filename)
+                    '''
+                    windowsでは"/"が"\"に置換されるため、「.filename」ではなく置換される前のデータが格納されている
+                    「.orig_filename」を使用。
+                    '''
+                    file.filename = file.orig_filename.encode('cp437').decode('cp932')
                 except UnicodeDecodeError as unideco_err:
                     print(unideco_err)
 
-                zip_file.extract(file, path=expansions_target, pwd=_password)
-            return
+                zip_file.extract(file, path=extract_target, pwd=_password)
+
 
 # read text
 def read_text(file_path):
@@ -37,16 +37,18 @@ def read_text(file_path):
     'category':'',
     'version':'',
     'product':'',
-    'description:':'',
-    'bugfix':''
+    'description:':''
     }
 
     LIST_INFO_CATEGORY = ['不具合情報','共通情報']
     AFTER_WORD_INFO_KEY = '(Ver'
 
-    with open(file_path, encoding='shift-jis', errors='strict') as opend_file:
+    with open(file_path, encoding='shift-jis', errors='strict', newline='\n') as opend_file:
         _title_range_flg = False
         _version_flg = False
+        _description_flg = False
+        _is_bugfix = False
+        _w_rows = ""
 
         try:
             for row in opend_file.readlines():
@@ -73,8 +75,10 @@ def read_text(file_path):
 
                             if category == '不具合情報':
                                 _word_top = row_striped.find(_category_name) + 7
+                                _is_bugfix = True
                             else:
-                                _word_top = row_striped.find(_category_name) + 6 
+                                _word_top = row_striped.find(_category_name) + 6
+                                _is_bugfix = False
 
                             _word_last = row_striped.find(AFTER_WORD_INFO_KEY)
 
@@ -82,10 +86,6 @@ def read_text(file_path):
                             dict_data['product'] = row_striped[_word_top:_word_last]
 
                             continue
-
-                
-
-
 
                 # version
                 if row_striped.startswith('＜対象バージョン＞'):
@@ -95,16 +95,31 @@ def read_text(file_path):
                 if _version_flg:
                     dict_data['version'] = row_striped.replace('Ver','')
                     _version_flg = False
-                
-                
+                    continue
 
                 # description
+                if _is_bugfix:
+                    if  row_striped.startswith('＜現象内容＞'):
+                        _description_flg = True
+                        continue
+
+                    if row_striped.startswith('＜適用方法＞'):
+                        dict_data['description'] = _w_rows
+                        _description_flg = False
+                        break
+
+                    if _description_flg:
+                        _w_rows += row_striped + '\n'
+                
+
+
+
                 # bugfix
             
-            print('category:', dict_data['category'])
-            print('title:', dict_data['title'])
-            print('product:', dict_data['product'])
-            print('version:', dict_data['version'])
+            # print('category:', dict_data['category'])
+            # print('title:', dict_data['title'])
+            # print('product:', dict_data['product'])
+            # print('version:', dict_data['version'])
             return dict_data
         except Exception as error:
             print(error)
@@ -119,28 +134,33 @@ if __name__ == '__main__':
     _txt_extension = 'txt'
     _xl_extension = 'xlsx'
     _dl_zip_dirname = 'sample'
+    _extract_target = 'extract'
+    _password = 'password'
     _current_dir = os.getcwd()
 
+    # zipファイル格納先パスを作成
     zip_dir = os.path.join(_current_dir, _dl_zip_dirname)
-
+    
+    # 格納されているzipファイルのパスリストを作成
     zip_paths = get_file_path_list(zip_dir, _comp_extension)
+
     if not zip_paths:
         print('No zip file.')
         sys.exit()
 
-    _expansions_target = 'extract'
-    _password = 'password'
-
+    # zipファイル展開
     try:
-        unzip(zip_paths, _password, _expansions_target)
+        unzip(zip_paths, _password, _extract_target)
     except Exception as error:
         print(error)
         sys.exit()
     
-    # get path list of folder made by extract function
-    expansions_dir_path = os.path.join(_current_dir, _expansions_target)
-    exp_dir = pathlib.Path(expansions_dir_path)
-    unzip_folders = [p for p in exp_dir.iterdir() if p.is_dir()]
+    # zipファイル展開先パスを作成
+    extract_dir_path = os.path.join(_current_dir, _extract_target)
+    
+    # zipから展開されたフォルダのパスリストを作成
+    ext_dir = pathlib.Path(extract_dir_path)
+    unzip_folders = [p for p in ext_dir.iterdir() if p.is_dir()]
     
     for folder in unzip_folders:
         files = get_file_path_list(str(folder),_txt_extension,_xl_extension)
@@ -148,3 +168,7 @@ if __name__ == '__main__':
         for file in files:
             if file.endswith('.txt'):
                 x = read_text(file)
+
+            from pprint import pprint
+            print('-' * 50)
+            print(x['description'])
